66687549c5634b24e4f9538e114c668aae6d1ce0
Denis Malyshev <malyshev_denis@outlook.com>
2021-01-15 20:48:02 +0400

    1

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+let position = null;
+let element = null;
+let previousElement = null;
+
+const collection = document.querySelectorAll('.field_li');
+
+ function random() {
+    let randomCell = Math.floor((Math.random() * (collection.length - 1 + 1)) + 1);
+    if(position === randomCell) {
+        random();
+    } else {
+        position = randomCell;
+        element = collection[position - 1];
+        if(previousElement !== null) previousElement.removeChild(previousElement.lastElementChild); // previousElement.innerHTML = '';
+        element.insertAdjacentHTML('beforeend', '<img src="goblin.png">');
+        previousElement = element;
+    }
+ }
+
+let int = setInterval(random, 1000); 
